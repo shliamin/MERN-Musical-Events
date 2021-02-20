288857e1f76546b97a625d75402afb7b8b8fa266
Efim Shliamin <shliamin@icloud.com>
2021-02-20 23:29:49 +0100

    implementing a basic desktop and mobile navigation in React

--- muapp/myWebapp/my-app/src/App.js
+++ muapp/myWebapp/my-app/src/App.js
@@ -77,21 +77,25 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom
 
 import Users from './users/pages/Users';
 import NewContact from './contacts/pages/NewContact';
+import MainNavigation from  './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-  return <Router>
-    <Switch>
-    <Route path="/" exact>
-    <Users/>
-
-    </Route>
-    <Route path="/contacts/new" exact>
-    <NewContact/>
-
-    </Route>
-    <Redirect to ="/" />
-    </Switch>
-  </Router>;
+  return (
+    <Router>
+      <MainNavigation />
+        <main >
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/contacts/new" exact>
+              <NewContact/>
+            </Route>
+            <Redirect to ="/" />
+          </Switch>
+        </main>
+    </Router>
+    );
   };
 
 
