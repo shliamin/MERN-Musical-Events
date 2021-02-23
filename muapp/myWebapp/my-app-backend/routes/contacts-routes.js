f1094ccfa365e6d069e9ce13d8069a5dd6941eb3
Efim Shliamin <shliamin@icloud.com>
2021-02-23 05:24:17 +0100

    backend: adding my own error model

--- muapp/myWebapp/my-app-backend/routes/contacts-routes.js
+++ muapp/myWebapp/my-app-backend/routes/contacts-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_CONTACTS = [
@@ -23,9 +25,7 @@ router.get('/:cid', (req,res,next)=>{
   });
 
   if(!contact) {
-    const error = new Error('Could not find a place for the provided id.');
-    error.code = 404;
-    throw error;
+    throw new HttpError('Could not find a place for the provided id.', 404);
   }
 
   res.json({contact}); // => {contact} => {contact: contact}
@@ -39,9 +39,9 @@ router.get('/user/:uid', (req,res,next) =>{
   });
 
   if(!contact) {
-    const error = new Error('Could not find a place for the provided user id.');
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    );
   }
 
   res.json({contact});
