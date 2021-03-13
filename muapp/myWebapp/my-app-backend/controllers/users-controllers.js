cce03a9ef57f913d6c7a839cd46a26a134a7deff
Efim Shliamin <shliamin@icloud.com>
2021-03-13 06:12:21 +0100

    Adding the relations between contacts and users

--- muapp/myWebapp/my-app-backend/controllers/users-controllers.js
+++ muapp/myWebapp/my-app-backend/controllers/users-controllers.js
@@ -35,7 +35,7 @@ const signup = async (req,res,next) => {
     );
   }
 
-  const {name,email,password,contacts} = req.body;
+  const {name,email,password} = req.body;
 
   let existingUser;
   try {
@@ -61,7 +61,7 @@ const signup = async (req,res,next) => {
     email,
     image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
     password,
-    contacts
+    contacts: []
   });
 
   try{
