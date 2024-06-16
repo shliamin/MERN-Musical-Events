527d27d1771b5f64663d73734536116a9c8c1df0
Efim <47456474+shliamin@users.noreply.github.com>
2024-06-16 02:27:45 +0200

    Delete models directory

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-  name: {type: String, required: true },
-  password: {type: String, required: true},
-  isAdmin: {type: Boolean, required: true}
-});
-
-module.exports = mongoose.model('User', userSchema);
