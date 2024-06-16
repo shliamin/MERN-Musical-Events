bd9a4860d5ba93e923eb9dd6fb7833d07e65793a
Efim <efimshliamin@gmail.com>
2024-06-17 00:04:00 +0200

    CSS changed

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -2,23 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
 
-import Backdrop from '../UIElements/Backdrop.js';  // Import the Backdrop component
 import './SideDrawer.css';
 
 const SideDrawer = props => {
   const content = (
-    <React.Fragment>
-      {props.show && <Backdrop onClick={props.onClick} />}  {/* Add the Backdrop component */}
-      <CSSTransition
-        in={props.show}
-        timeout={200}
-        classNames="slide-in-left"
-        mountOnEnter
-        unmountOnExit
-      >
-        <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
-      </CSSTransition>
-    </React.Fragment>
+  <CSSTransition
+  in={props.show}
+  timeout={200}
+  classNames="slide-in-left"
+  mountOnEnter
+  unmountOnExit
+  >
+    <aside className="side-drawer">{props.children}</aside>
+  </CSSTransition>
   );
 
   return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
